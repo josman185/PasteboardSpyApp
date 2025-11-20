d0890e41a2d84222c27644e6eb12bf95a6bb29e7
josevarflo <jose.vargasflores@outlook.com>
2025-11-19 22:26:48 -0600

    configure UIPasteboard

--- Pasteboard/UITextField+Ext.swift
+++ Pasteboard/UITextField+Ext.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2025 Nyisztor, Karoly. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    override open func copy(_ sender: Any?) {
+        AppDelegate.customPasteBoard?.string = self.text
+    }
+    
+    override open func cut(_ sender: Any?) {
+        AppDelegate.customPasteBoard?.string = self.text
+    }
+    
+    override open func paste(_ sender: Any?) {
+        if let text = AppDelegate.customPasteBoard?.string {
+            self.text = text
+        }
+    }
+}
