c09ab1fe72d8e80f4a6c4b27c0b52f505d286451
josevarflo <jose.vargasflores@outlook.com>
2025-11-26 21:20:19 -0600

    Clearing Data before moving to the background

--- Pasteboard/ViewController.swift
+++ Pasteboard/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.UIApplicationWillResignActive, object: nil, queue: OperationQueue.main) { _ in
+            for view in self.view.subviews {
+                if let textField = view as? UITextField {
+                    textField.text = nil
+                }
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+        super.viewWillDisappear(animated)
     }
 
     override func didReceiveMemoryWarning() {
